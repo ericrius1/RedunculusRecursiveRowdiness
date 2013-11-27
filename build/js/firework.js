2100649abd6b87b53e77de0a13d3e6f8c2e2649d
Eric <Ericrius1@gmail.com>
2013-11-27 13:44:49 -0800

    mult lights

--- build/js/firework.js
+++ build/js/firework.js
@@ -8,50 +8,58 @@
 
     function Firework() {
       var i, _i;
-      this.groups = [];
       this.colorStart = new THREE.Color();
       this.colorEnd = new THREE.Color();
       this.numFireworksPerExplosion = 3;
+      this.lightIndex = 0;
       this.explodeSound = new Audio('./assets/explosion.mp3');
       this.crackleSound = new Audio('./assets/crackle.mp3');
+      this.lights = [];
       this.particleGroup = new ShaderParticleGroup({
         texture: THREE.ImageUtils.loadTexture('assets/smokeparticle.png'),
         blending: THREE.AdditiveBlending,
-        maxAge: 8
+        maxAge: rnd(5, 15)
       });
-      for (i = _i = 1; _i <= 25; i = ++_i) {
+      for (i = _i = 1; _i <= 10; i = ++_i) {
         this.particleGroup.addPool(1, this.generateEmitter(), true);
       }
       FW.scene.add(this.particleGroup.mesh);
     }
 
     Firework.prototype.generateEmitter = function() {
-      var emitterSettings;
+      var emitterSettings, light;
       this.colorStart.setRGB(Math.random(), Math.random(), Math.random());
       this.colorEnd.setRGB(Math.random(), Math.random(), Math.random());
+      light = new THREE.PointLight(this.colorStart, 0.0, 4000);
+      FW.scene.add(light);
+      this.lights.push(light);
       return emitterSettings = {
-        size: rnd(0.01, 0.3),
-        velocity: new THREE.Vector3(0, rnd(-2, 2), 0),
+        size: rnd(0.01, 1.3),
+        velocity: new THREE.Vector3(rnd(-2, 2), rnd(-2, 2), rnd(-2, 2)),
         acceleration: new THREE.Vector3(0, -0.01, 0),
-        accelerationSpread: new THREE.Vector3(rnd(0, 5), rnd(0, 5), rnd(0, 5)),
+        accelerationSpread: new THREE.Vector3(rnd(-5, 5), rnd(-5, 5), rnd(-5, 5)),
         colorStart: this.colorStart,
-        colorSpread: new THREE.Vector3(.3, .3, .3),
+        colorSpread: new THREE.Vector3(rnd(.1, .5), rnd(.1, .5), rnd(.1, .5)),
         colorEnd: this.colorEnd,
         particlesPerSecond: rnd(100, 500),
         alive: 0,
-        emitterDuration: 3.0
+        emitterDuration: rnd(1.0, 5.0)
       };
     };
 
     Firework.prototype.createExplosion = function(origPos, newPos, count) {
-      var _this = this;
+      var emitter, light,
+        _this = this;
       if (newPos == null) {
         newPos = origPos;
       }
       if (count == null) {
         count = 0;
       }
-      this.particleGroup.triggerPoolEmitter(2, newPos);
+      emitter = this.particleGroup.triggerPoolEmitter(1, newPos);
+      light = this.lights[this.lightIndex++];
+      light.position.set(newPos.x, newPos.y, newPos.z);
+      light.intensity = 1.0;
       if (count < this.numFireworksPerExplosion) {
         return setTimeout(function() {
           _this.explodeSound.load();
@@ -64,12 +72,24 @@
           count++;
           newPos = new THREE.Vector3(rnd(origPos.x - 20, origPos.x + 20), rnd(origPos.y - 20, origPos.y + 20), rnd(origPos.z - 20, origPos.z + 20));
           return _this.createExplosion(origPos, newPos, count++);
-        }, rnd(100, 500));
+        }, rnd(100, 700));
       }
     };
 
     Firework.prototype.tick = function() {
-      return this.particleGroup.tick(0.16);
+      var light, _i, _len, _ref, _results;
+      this.particleGroup.tick(0.16);
+      _ref = this.lights;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        light = _ref[_i];
+        if (light.intensity > 0) {
+          _results.push(light.intensity -= 0.01);
+        } else {
+          _results.push(void 0);
+        }
+      }
+      return _results;
     };
 
     return Firework;
--- src/firework.coffee
+++ src/firework.coffee
@@ -1,50 +1,56 @@
 FW.Firework = class Firework
   rnd = FW.rnd
   constructor: ()->
-    @groups = []
     #create a few different emmitters and add to pool
     @colorStart = new THREE.Color()
     @colorEnd = new THREE.Color()
     @numFireworksPerExplosion = 3
+    @lightIndex = 0
 
     @explodeSound = new Audio('./assets/explosion.mp3');
     @crackleSound = new Audio('./assets/crackle.mp3');
-    
+    @lights = []
+
 
 
     
     @particleGroup = new ShaderParticleGroup({
       texture: THREE.ImageUtils.loadTexture('assets/smokeparticle.png'),
       blending: THREE.AdditiveBlending,
-      maxAge: 8
+      maxAge: rnd(5, 15)
     });
 
-    for i in [1..25]
+    for i in [1..10 ]
       @particleGroup.addPool( 1, @generateEmitter(), true )     
     FW.scene.add(@particleGroup.mesh)
 
   generateEmitter : ->
     @colorStart.setRGB(Math.random(), Math.random(),Math.random())
     @colorEnd.setRGB(Math.random(), Math.random(),Math.random())
+    light = new THREE.PointLight(@colorStart, 0.0, 4000)
+    FW.scene.add(light)
+    @lights.push(light)
     emitterSettings = 
-      size: rnd(0.01, 0.3),
-      velocity: new THREE.Vector3(0, rnd(-2, 2), 0)
+      size: rnd(0.01, 1.3),
+      velocity: new THREE.Vector3(rnd(-2, 2), rnd(-2, 2), rnd(-2, 2))
       acceleration: new THREE.Vector3(0, -0.01, 0),
-      accelerationSpread: new THREE.Vector3(rnd(0, 5), rnd(0, 5), rnd(0, 5)),
+      accelerationSpread: new THREE.Vector3(rnd(-5, 5), rnd(-5, 5), rnd(-5, 5)),
       colorStart: @colorStart,
-      colorSpread: new THREE.Vector3(.3, .3, .3),
+      colorSpread: new THREE.Vector3(rnd(.1, .5), rnd(.1, .5), rnd(.1, .5)),
       colorEnd: @colorEnd,
       particlesPerSecond: rnd(100, 500),
       alive: 0,  
-      emitterDuration: 3.0
+      emitterDuration: rnd(1.0, 5.0)
 
   createExplosion: (origPos, newPos = origPos, count=0)->
-    @particleGroup.triggerPoolEmitter(2, newPos)
+    emitter = @particleGroup.triggerPoolEmitter(1, newPos)
+    light = @lights[@lightIndex++]
+    light.position.set(newPos.x, newPos.y, newPos.z)
+    light.intensity = 1.0
     if count < @numFireworksPerExplosion
       setTimeout =>
         @explodeSound.load()
-            #set timeout for speed of sound delay!
-
+        #set timeout for speed of sound delay!
         setTimeout(()=>
           @explodeSound.play()
           setTimeout(()=>
@@ -54,7 +60,12 @@ FW.Firework = class Firework
         count++
         newPos = new THREE.Vector3(rnd(origPos.x - 20, origPos.x+20), rnd(origPos.y - 20, origPos.y+20), rnd(origPos.z - 20, origPos.z+20))
         @createExplosion(origPos, newPos, count++)
-      ,rnd(100, 500)
+      ,rnd(100, 700)
+
     
   tick: ->
     @particleGroup.tick(0.16)
+    for light in @lights
+      if light.intensity > 0
+        light.intensity -=0.01
+
