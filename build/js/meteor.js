3e7be61bcf13c9bcb99e7f016ef40bf830840d08
Eric <Ericrius1@gmail.com>
2013-11-30 17:54:33 -0800

    shnur

--- build/js/meteor.js
+++ build/js/meteor.js
@@ -20,12 +20,10 @@
       this.emitterSettings = {
         size: rnd(0.01, 1.3),
         sizeSpread: rnd(0.1, 1.0),
-        velocity: new THREE.Vector3(rnd(-1, 1), rnd(-1, 1), rnd(-1, 1)),
-        acceleration: new THREE.Vector3(0, -0.01, 0),
-        accelerationSpread: new THREE.Vector3(rnd(-2, 0), rnd(-2, 0), rnd(-2, 0)),
+        acceleration: new THREE.Vector3(1, 0, 0),
         particlesPerSecond: rnd(100, 500),
         alive: 0,
-        emitterDuration: rnd(1.0, 5.0),
+        emitterDuration: 100,
         opacityEnd: 0.5
       };
       this.meteorTail.addPool(5, this.emitterSettings, true);
@@ -34,10 +32,10 @@
 
     Meteor.prototype.startShower = function() {
       var _this = this;
-      return setInterval(function() {
+      return setTimeout(function() {
         console.log("Spakrle!!!!");
         return _this.meteorTail.triggerPoolEmitter(1, _this.meteorHead.position);
-      }, 5000);
+      }, 1000);
     };
 
     Meteor.prototype.tick = function() {
--- src/meteor.coffee
+++ src/meteor.coffee
@@ -18,21 +18,20 @@ FW.Meteor = class Meteor
     @emitterSettings = 
       size: rnd(0.01, 1.3),
       sizeSpread: rnd(0.1, 1.0),
-      velocity: new THREE.Vector3(rnd(-1, 1), rnd(-1, 1), rnd(-1, 1))
-      acceleration: new THREE.Vector3(0, -0.01, 0),
-      accelerationSpread: new THREE.Vector3(rnd(-2, 0), rnd(-2, 0), rnd(-2, 0)),
+      acceleration: new THREE.Vector3(1, 0, 0),
+      accelerationSpread(.1, .1, .1),
       particlesPerSecond: rnd(100, 500),
       alive: 0,  
-      emitterDuration: rnd(1.0, 5.0)
+      emitterDuration: 100
       opacityEnd: 0.5
     @meteorTail.addPool(5, @emitterSettings, true)
     FW.scene.add(@meteorTail.mesh)
     
   startShower: ->
-    setInterval(()=>
+    setTimeout(()=>
       console.log "Spakrle!!!!"
       @meteorTail.triggerPoolEmitter(1, @meteorHead.position)
-    5000) 
+    000) 
 
     
   tick: ->
