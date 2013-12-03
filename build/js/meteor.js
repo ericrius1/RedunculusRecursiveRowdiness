8352167d7707375a929b79862a2d9a9e6b70ceae
Eric <Ericrius1@gmail.com>
2013-12-03 15:44:33 -0800

    shmnur

--- build/js/meteor.js
+++ build/js/meteor.js
@@ -8,7 +8,8 @@
 
     function Meteor() {
       var sphereGeo;
-      this.speed = 5;
+      this.speed = .1;
+      this.dir = rnd(-1, 1);
       this.meteorTail = new ShaderParticleGroup({
         texture: THREE.ImageUtils.loadTexture('assets/star.png'),
         blending: THREE.AdditiveBlending,
@@ -17,32 +18,28 @@
       sphereGeo = new THREE.SphereGeometry(5, 5, 5);
       this.meteorHead = new THREE.Mesh(sphereGeo, FW.rocketMat);
       this.meteorHead.position.set(-992, 820, 1165);
-      FW.scene.add(this.meteorHead);
-      this.emitterSettings = {
+      this.emitter = new ShaderParticleEmitter({
+        position: new THREE.Vector3(-992, 820, 1165),
         size: rnd(0.01, 1.3),
         sizeSpread: rnd(0.1, 1.0),
         acceleration: new THREE.Vector3(1, 0, 0),
         accelerationSpread: new THREE.Vector3(.1, .1, .1),
         particlesPerSecond: rnd(100, 500),
-        alive: 0,
-        emitterDuration: 100,
         opacityEnd: 0.5
-      };
-      this.meteorTail.addPool(5, this.emitterSettings, true);
+      });
+      this.meteorTail.addEmitter(this.emitter);
       FW.scene.add(this.meteorTail.mesh);
     }
 
     Meteor.prototype.startShower = function() {
       var _this = this;
-      return setTimeout(function() {
-        console.log("Spakrle!!!!");
-        return _this.meteorTail.triggerPoolEmitter(1, _this.meteorHead.position);
-      });
+      return setTimeout(function() {}, 500);
     };
 
     Meteor.prototype.tick = function() {
-      console.log("POSITION", this.meteorHead.position.x);
-      this.meteorHead.translateX(this.speed * 1);
+      this.meteorHead.translateX(this.speed * this.dir);
+      this.meteorHead.translateY(this.speed * this.dir);
+      this.emitter.position.x = this.meteorHead.position.x;
       return this.meteorTail.tick(0.16);
     };
 
--- src/meteor.coffee
+++ src/meteor.coffee
@@ -1,7 +1,8 @@
 FW.Meteor = class Meteor
   rnd = FW.rnd
   constructor: ()->
-    @speed = 5
+    @speed = .1
+    @dir = rnd(-1, 1)
     #create a few different emmitters and add to pool
 
     
@@ -14,30 +15,28 @@ FW.Meteor = class Meteor
     sphereGeo = new THREE.SphereGeometry(5, 5, 5 )
     @meteorHead = new THREE.Mesh(sphereGeo, FW.rocketMat)
     @meteorHead.position.set -992, 820, 1165
-    FW.scene.add(@meteorHead)
+    # FW.scene.add(@meteorHead)
 
-    @emitterSettings =
+    @emitter = new ShaderParticleEmitter
+      position: new THREE.Vector3(-992, 820, 1165)
       size: rnd(0.01, 1.3),
       sizeSpread: rnd(0.1, 1.0),
       acceleration: new THREE.Vector3(1, 0, 0),
       accelerationSpread: new THREE.Vector3(.1, .1, .1),
       particlesPerSecond: rnd(100, 500),
-      alive: 0,  
-      emitterDuration: 100,
       opacityEnd: 0.5
-    @meteorTail.addPool(5, @emitterSettings, true)
+    @meteorTail.addEmitter @emitter
     FW.scene.add(@meteorTail.mesh)
     
   startShower: ->
     setTimeout(()=>
-      console.log "Spakrle!!!!"
-      @meteorTail.triggerPoolEmitter(1, @meteorHead.position)
-    ) 
+    500)
 
     
   tick: ->
-    console.log "POSITION", @meteorHead.position.x
-    @meteorHead.translateX(@speed * 1)
+    @meteorHead.translateX(@speed * @dir)
+    @meteorHead.translateY(@speed * @dir)
+    @emitter.position.x = @meteorHead.position.x
     @meteorTail.tick(0.16)
     
 
